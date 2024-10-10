060068de8f0c56120b766293cd1f6963424e1a59
Albano Tjyakala <alb4no9@gmail.com>
2024-10-10 19:02:43 +0100

    sos_button

---
+++ sos_button.js
@@ -0,0 +1,9 @@
+function sosRequest() {
+    // Exibe o alerta personalizado
+    document.getElementById('customAlert').style.display = 'flex';
+}
+
+function closeCustomAlert() {
+    // Fecha o alerta personalizado
+    document.getElementById('customAlert').style.display = 'none';
+}
