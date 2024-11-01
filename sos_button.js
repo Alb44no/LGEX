30f8d58fe4cfcbbe287117f79b842aa12296d16f
Albano Tjyakala <alb4no9@gmail.com>
2024-11-01 20:49:49 +0000

    botom update

--- sos_button.js
+++ sos_button.js
@@ -1,9 +1,13 @@
 function sosRequest() {
-    // Exibe o alerta personalizado
-    document.getElementById('customAlert').style.display = 'flex';
+    // Verifica se o usuário está em um dispositivo móvel
+    if (/Mobi|Android/i.test(navigator.userAgent)) {
+        window.location.href = "tel:+351936752249"; // Liga diretamente para o número
+    } else {
+        // Exibe o alerta personalizado na versão web
+        document.getElementById("customAlert").style.display = "block";
+    }
 }
 
 function closeCustomAlert() {
-    // Fecha o alerta personalizado
-    document.getElementById('customAlert').style.display = 'none';
+    document.getElementById("customAlert").style.display = "none";
 }
